979e66504c80ae40a5e635a673314296e2d80cc4
yanhhhhhh <1801084586@qq.com>
2024-09-18 23:28:46 +0800

    feat:路由中间件

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { LogMiddleware } from './log.middleware';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LogMiddleware).forRoutes('product*'); // 应用中间件
+  }
+}
